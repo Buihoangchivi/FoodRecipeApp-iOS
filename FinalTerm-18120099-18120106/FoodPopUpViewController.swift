27ad908d65e71101ddbccaa35a4a994d5429728f
Buihoangchivi <buivankhang9@gmail.com>
2020-12-09 23:18:18 +0700

    Xu ly them thuc don len Firebase khi nhan nut 'Chon mon' trong man hinh chi tiet mon an.

--- FinalTerm-18120099-18120106/FoodPopUpViewController.swift
+++ FinalTerm-18120099-18120106/FoodPopUpViewController.swift
@@ -106,18 +106,6 @@ class FoodPopUpViewController: UIViewController {
         dest.delegate = self
         self.present(dest, animated: true, completion: nil)
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension FoodPopUpViewController: DatePickerDalegate{
