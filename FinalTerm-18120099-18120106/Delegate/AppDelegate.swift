40f37c06d32856c3d297ac248ba628fc0fce6f92
Buihoangchivi <buivankhang9@gmail.com>
2021-01-23 20:22:38 +0700

    Thêm icon và launch screen cho ứng dụng.

--- FinalTerm-18120099-18120106/Delegate/AppDelegate.swift
+++ FinalTerm-18120099-18120106/Delegate/AppDelegate.swift
@@ -15,6 +15,9 @@ import FBSDKCoreKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        Thread.sleep(forTimeInterval: 1.5)
+        
         FirebaseApp.configure()
         GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
         ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
